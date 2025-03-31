434b0b9177daacfb2dd797f11f13256aee20f230
Om Berde <omberde0@gmail.com>
2025-03-31 17:15:17 +0530

    Project Ended | Finalized

--- obj/Debug/net8.0/MinesGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinesGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinesGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40b68cd154671f3c2589e622d0ecb7bdc97a1790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97624e5e7ec934f1ecb6e6355a14a2003c639c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinesGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinesGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
